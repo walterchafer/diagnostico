6d3ed5cf790a40d4374c552b166a4adce5a1c4d7
Walter Chafer <walterchafer@gmail.com>
2018-10-05 17:46:20 -0300

    se modificaron el inex, estilo.css y accion.js

--- js/accion.js
+++ js/accion.js
@@ -1,16 +1,41 @@
-function principal()
-{
-  var inp = document.getElementsByTagName("input");
+function principal() {
+  var inp = document.getElementsByTagName('input');
   inp.bot.onclick = validar;
 
-  function Validar() {
-     var c1 = document.getElementById('s1').checked;
-    //var c2 = document.getElementById('s2').checked;
-    //var c3 = document.getElementById('s3').checked;
-    //var c4 = document.getElementById('s4').checked;
+  a = 'Fiebre';
 
-    if (c1==true) {
-      alert("Te gustan las Naranjas");
+  function validar() {
+
+    var c1 = document.getElementById('s1').checked;
+    var c2 = document.getElementById('s2').checked;
+    var c3 = document.getElementById('s3').checked;
+    var c4 = document.getElementById('s4').checked;
+
+    if (c1 == true) {
+      
+      //a = "fiebre";
+      //alert('Seleccionaste el sintoma: ' + a);
+      //var gripe = ['Fiebre', 'temperatura', 'diarrea', 'mobile', 'cual', 'piuma', 'al', 'vento'];
+      //if (Array.gripe.includes('Fiebre')) {
+      //  alert('el array gripe incluye fiebre');
+    } else {
+      alert('No te gustan las Naranjas');
+    }
+
+    if (c2 == true) {
+      alert('Te gustan las Naranjas');
+    } else {
+      alert('No te gustan las Naranjas');
+    }
+
+    if (c3 == true) {
+      alert('Te gustan las Naranjas');
+    } else {
+      alert('No te gustan las Naranjas');
+    }
+
+    if (c4 == true) {
+      alert('Te gustan las Naranjas');
     } else {
       alert('No te gustan las Naranjas');
     }
